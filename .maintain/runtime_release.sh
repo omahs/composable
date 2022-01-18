af0c9fd80b1e293faefb218ce4395afb227e35fa
haroldsphinx <adedayoakinpelu@gmail.com>
2022-01-18 21:12:56 +0100

    fixing error with release-note

--- .maintain/runtime_release.sh
+++ .maintain/runtime_release.sh
@@ -25,7 +25,7 @@ cargo install --locked --git https://github.com/chevdor/subwasm --tag v0.16.1
 build_runtime () {
   chain=$3
   # srtool for reproducible builds
-  srtool build --package "$chain"-runtime --profile release --runtime-dir runtime/"$chain"
+  srtool build --offline  --package "$chain"-runtime --profile release --runtime-dir runtime/"$chain"
   # subwasm for runtime metadata
   echo "# $chain Runtime " >> release.md
   subwasm info ./runtime/"$chain"/target/srtool/release/wbuild/"$chain"-runtime/"$chain"_runtime.compact.wasm >> release.md
