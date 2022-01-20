1b8c0330640290e12cf5b3708ba9695b201ea59e
Ewere Diagboya <nimboya@gmail.com>
2022-01-20 22:15:49 +0100

    update release note

--- .maintain/runtime_release.sh
+++ .maintain/runtime_release.sh
@@ -28,8 +28,8 @@ build_runtime () {
   srtool build --package "$chain"-runtime --profile release --runtime-dir ./runtime/"$chain"
   # subwasm for runtime metadata
   echo "# $chain Runtime" >> release.md
-  wasmout=subwasm info ./runtime/"$chain"/target/srtool/release/wbuild/"$chain"-runtime/"$chain"_runtime.compact.wasm #>> release.md
-  echo "<code>${wasmout}</code>" >> release.md
+  subwasm info ./runtime/"$chain"/target/srtool/release/wbuild/"$chain"-runtime/"$chain"_runtime.compact.wasm >> release.md
+  #echo "<code>${wasmout}</code>" >> release.md
 }
 
 # Check which runtimes have changed and build them
