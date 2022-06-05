6758e2181f6c85609b2b27367fdd15fa3315ec44
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-06-05 22:04:56 +0100

    round up

--- frame/composable-traits/src/nft/mod.rs
+++ frame/composable-traits/src/nft/mod.rs
@@ -1,3 +1,35 @@
+//! Default interface used to interact with financial NFTs through a NFT provider.
+//! 
+//! Allows  allows to enter positions and sell it before position is left as NFT.
+//! Allows to split financial positions.
+//! 
+//! # Design
+//! There are 2 ways to integrate NFT and financial positions.
+//! 
+//! Store all metadata of position in NFT BLOB or store only reference in NFT.
+//! 
+//! In case of storing reference,
+//! - NFT account produced from identity is set as owner of position
+//! - protocol providing position implements trait which allow to work with it through NFT API
+//! - position is stored in any way efficient for that
+//! - NFT stores only reference to position in protocol, example pallet identifier and some monotonous identifier
+//! - during XCMP transfer, owner of NFT become target parachain
+//! - allow to build NFTs from lower level typed positions
+//! 
+//! So NFT is (class, id, position reference)
+//! 
+//! 
+//! In case of storing all data in NFT, 
+//! -  protocol calls NFT to read state 
+//! - storage is not maximally efficient
+//! - more risks of having serde issue and types
+//! 
+//! In both cases XCMP NFT state as whole requires protocol constraints, like immutability or protocol implementation on other side.   
+//! In both cases RPC or shared library is required to interpret state offchain.
+//! 
+//! Problem with NFT as it is considered isolated, while burn, split, mint in protocols influence total/shared.
+//! 
+//! 
 pub mod protocol;
 
 
@@ -16,24 +48,21 @@ use scale_info::TypeInfo;
 use sp_runtime::{DispatchError, TokenError, Permill};
 use sp_std::{collections::btree_map::BTreeMap, vec::Vec};
 
-pub type AttributeKey = BiBoundedVec<u8, 1, 64>;
-pub type AttributeValue = BiBoundedVec<u8, 1, 256>; 
+/// depending on `ClassId` this can mean typed position or complex storage in NFT
+pub type Reference = BiBoundedVec<u8, 1, 64>;
 
-pub trait FinancialNftProvider<AccountId>: Create<AccountId> + Mutate<AccountId> + Inspect<AccountId> {
-    fn protocol_mint_into<NFTProvider, NFT>(
+
+/// allow to wrap any position into ownership of fFNT
+pub trait ReferenceNft<AccountId>: Create<AccountId> + Mutate<AccountId> + Inspect<AccountId> {
+    // `who` must be owner of original reference. after NFTing position, NFT instance is owner, but `who` is owner of NFT
+    // in case of reference NFT is reported burn (so owner is lost), it is auctioned
+    fn reference_mint_into<NFTProvider, NFT>(
 		_class: &Self::ClassId,
 		_instance: &Self::InstanceId,
-        nft: NFT,
 		_who: &AccountId,
+        reference: Reference,
 	) -> DispatchResult;
-
-
-    fn protocol_split_into<NFTProvider, NFT>(
-		instance: &Self::InstanceId,
-		parts: BiBoundedVec<Permill, 1, 16>,
-	) -> Result<BiBoundedVec<Self::InstanceId, 1, 16>, DispatchError>;
 }
-
 
 /// Default ClassId type used for NFTs.
 #[derive(
