a8364c691710d5d614d114e48ccddd2ce0856c93
Matteocolella1995 <84871477+Matteocolella1995@users.noreply.github.com>
2022-06-28 14:26:53 +0000

    CU-31zumbf - Started tests for exercise_option extrinsic

--- frame/tokenized-options/src/tests/exercise_option.rs
+++ frame/tokenized-options/src/tests/exercise_option.rs
@@ -28,10 +28,23 @@ use sp_runtime::ArithmeticError;
 //		Exercise Options Tests
 // ----------------------------------------------------------------------------------------------------
 pub fn exercise_option_success_checks(option_id: AssetId, option_amount: Balance, who: Public) {
-	// Get info before extrinsic for checks
-	let _option = OptionIdToOption::<MockRuntime>::get(option_id).unwrap();
+	let option = OptionIdToOption::<MockRuntime>::get(option_id).unwrap();
 
-	// Call extrinsic
+	// Different behaviors based on Call or Put option
+	let asset_id = match option.option_type {
+		OptionType::Call => option.base_asset_id,
+		OptionType::Put => option.quote_asset_id,
+	};
+	// ---------------------------
+	// |  Data before extrinsic  |
+	// ---------------------------
+	let protocol_account = TokenizedOptions::account_id(asset_id);
+	let initial_total_issuance = Assets::total_issuance(option_id);
+	let initial_user_balance_options = Assets::balance(option_id, &who);
+	let initial_user_balance = Assets::balance(asset_id, &who);
+	let initial_protocol_balance = Assets::balance(asset_id, &protocol_account);
+
+	// Call extrinsic and check event
 	assert_ok!(TokenizedOptions::exercise_option(Origin::signed(who), option_amount, option_id));
 
 	// Check correct event
@@ -40,16 +53,42 @@ pub fn exercise_option_success_checks(option_id: AssetId, option_amount: Balance
 		option_amount,
 		option_id,
 	}));
+
+	// ---------------------------
+	// |  Data after extrinsic  |
+	// ---------------------------
+	// Check buyer balance after exercise has increased
+	assert_eq!(
+		Assets::balance(asset_id, &who),
+		initial_user_balance + option.exercise_amount * option_amount
+	);
+
+	// Check protocol balance after exercise is correct
+	assert_eq!(
+		Assets::balance(asset_id, &protocol_account),
+		initial_protocol_balance - option.exercise_amount * option_amount
+	);
+
+	// Check user owns the correct issuance of option token
+	assert_eq!(Assets::balance(option_id, &who), initial_user_balance_options - option_amount);
+
+	// Check position is updated correctly
+	let updated_total_issuance = Assets::total_issuance(option_id);
+	assert_eq!(updated_total_issuance, initial_total_issuance - option_amount);
 }
 
 #[test]
 fn test_exercise_option_with_initialization_success() {
 	ExtBuilder::default()
 		.initialize_balances(Vec::from([
-			(ALICE, BTC, 1 * UNIT),
-			(ALICE, USDC, 50000 * UNIT),
-			(BOB, BTC, 1 * UNIT),
-			(BOB, USDC, 50000 * UNIT),
+			(ALICE, BTC, 10 * UNIT),
+			(ALICE, USDC, 500000 * UNIT),
+			(BOB, BTC, 10 * UNIT),
+			(BOB, USDC, 500000 * UNIT),
+			(CHARLIE, BTC, 10 * UNIT),
+			(CHARLIE, USDC, 500000 * UNIT),
+			(DAVE, BTC, 10 * UNIT),
+			(DAVE, USDC, 500000 * UNIT),
 		]))
 		.build()
 		.initialize_oracle_prices()
@@ -70,7 +109,8 @@ fn test_exercise_option_with_initialization_success() {
 			));
 
 			// Create default BTC option
-			let option_config = OptionsConfigBuilder::default().build();
+			let option_config =
+				OptionsConfigBuilder::default().option_type(OptionType::Call).build();
 
 			assert_ok!(TokenizedOptions::create_option(
 				Origin::signed(ADMIN),
@@ -88,26 +128,153 @@ fn test_exercise_option_with_initialization_success() {
 			);
 
 			// Check creation ended correctly
-			assert!(OptionHashToOptionId::<MockRuntime>::contains_key(option_hash));
 			let option_id = OptionHashToOptionId::<MockRuntime>::get(option_hash).unwrap();
 
 			// Sell option and make checks
-			let option_amount = 1u128;
-			sell_option_success_checks(option_id, option_amount, BOB);
+			let alice_option_amount = 5u128;
+			let bob_option_amount = 4u128;
+			let charlie_option_amount = 3u128;
+			let dave_option_amount = 6u128;
+
+			assert_ok!(TokenizedOptions::sell_option(
+				Origin::signed(ALICE),
+				alice_option_amount,
+				option_id
+			));
+
+			assert_ok!(TokenizedOptions::sell_option(
+				Origin::signed(BOB),
+				bob_option_amount,
+				option_id
+			));
 
 			// Go to purchase window
 			run_to_block(3);
 
 			// Buy option
-			buy_option_success_checks(option_id, option_amount, ALICE);
+			assert_ok!(TokenizedOptions::buy_option(
+				Origin::signed(CHARLIE),
+				charlie_option_amount,
+				option_id
+			));
 
-			// BTC price moves from 50k to 55k
+			assert_ok!(TokenizedOptions::buy_option(
+				Origin::signed(DAVE),
+				dave_option_amount,
+				option_id
+			));
+
+			// BTC price moves from 50k to 55k, buyers are in profit
 			set_oracle_price(option_config.base_asset_id, 55000u128 * UNIT);
 
-			// Go to exercise window
+			// Go to exercise window (option has expired so settlement can start)
 			run_to_block(6);
 
-			// Exercise option
-			exercise_option_success_checks(option_id, option_amount, ALICE);
+			// Settle options
+			assert_ok!(TokenizedOptions::settle_options());
+
+			exercise_option_success_checks(option_id, charlie_option_amount, CHARLIE);
+			exercise_option_success_checks(option_id, dave_option_amount, DAVE);
+
+			// Check position is updated correctly
+			let updated_total_issuance = Assets::total_issuance(option_id);
+			assert_eq!(updated_total_issuance, 0u128);
+
+			// Check protocol balance after exercise is correct
+			let protocol_account = TokenizedOptions::account_id(BTC);
+			assert_eq!(Assets::balance(BTC, &protocol_account), 0u128);
+		});
+}
+
+#[test]
+fn test_exercise_option_success() {
+	ExtBuilder::default()
+		.initialize_balances(Vec::from([
+			(ALICE, BTC, 10 * UNIT),
+			(ALICE, USDC, 500000 * UNIT),
+			(BOB, BTC, 10 * UNIT),
+			(BOB, USDC, 500000 * UNIT),
+			(CHARLIE, BTC, 10 * UNIT),
+			(CHARLIE, USDC, 500000 * UNIT),
+			(DAVE, BTC, 10 * UNIT),
+			(DAVE, USDC, 500000 * UNIT),
+		]))
+		.build()
+		.initialize_oracle_prices()
+		.initialize_all_vaults()
+		.initialize_all_options()
+		.execute_with(|| {
+			let option_config =
+				OptionsConfigBuilder::default().option_type(OptionType::Call).build();
+
+			let option_hash = TokenizedOptions::generate_id(
+				option_config.base_asset_id,
+				option_config.quote_asset_id,
+				option_config.base_asset_strike_price,
+				option_config.quote_asset_strike_price,
+				option_config.option_type,
+				option_config.expiring_date,
+				option_config.exercise_type,
+			);
+
+			assert!(OptionHashToOptionId::<MockRuntime>::contains_key(option_hash));
+			let option_id = OptionHashToOptionId::<MockRuntime>::get(option_hash).unwrap();
+
+			// Sell option and make checks
+			let alice_option_amount = 10u128;
+			let bob_option_amount = 8u128;
+			let charlie_option_amount = 4u128;
+			let dave_option_amount = 7u128;
+
+			assert_ok!(TokenizedOptions::sell_option(
+				Origin::signed(ALICE),
+				alice_option_amount,
+				option_id
+			));
+			let protocol_account = TokenizedOptions::account_id(BTC);
+
+			assert_ok!(TokenizedOptions::sell_option(
+				Origin::signed(BOB),
+				bob_option_amount,
+				option_id
+			));
+
+			// Go to purchase window
+			run_to_block(3);
+
+			// Buy option
+			assert_ok!(TokenizedOptions::buy_option(
+				Origin::signed(CHARLIE),
+				charlie_option_amount,
+				option_id
+			));
+
+			assert_ok!(TokenizedOptions::buy_option(
+				Origin::signed(DAVE),
+				dave_option_amount,
+				option_id
+			));
+
+			// BTC price moves from 50k to 55k, buyers are in profit
+			set_oracle_price(option_config.base_asset_id, 55000u128 * UNIT);
+
+			// Go to exercise window (option has expired so settlement can start)
+			run_to_block(6);
+
+			// Settle options
+			assert_ok!(TokenizedOptions::settle_options());
+			let protocol_amount = Assets::balance(BTC, &protocol_account);
+
+			exercise_option_success_checks(option_id, charlie_option_amount, CHARLIE);
+			let protocol_amount = Assets::balance(BTC, &protocol_account);
+
+			exercise_option_success_checks(option_id, dave_option_amount, DAVE);
+
+			// Check position is updated correctly
+			let updated_total_issuance = Assets::total_issuance(option_id);
+			assert_eq!(updated_total_issuance, 0u128);
+
+			// Check protocol balance after exercise is correct
+			assert_eq!(Assets::balance(BTC, &protocol_account), 0u128);
 		});
 }
