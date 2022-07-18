f8c39d5746b5436ddc85427b95bf8b4961814e09
Matteocolella1995 <84871477+Matteocolella1995@users.noreply.github.com>
2022-07-18 15:29:11 +0000

    CU-3703gb9 - Started benchmark tests for create_asset_vault extrinsic

--- frame/tokenized-options/src/benchmarking.rs
+++ frame/tokenized-options/src/benchmarking.rs
@@ -2,23 +2,48 @@
 // #![cfg(feature = "runtime-benchmarks")]
 
 use crate::*;
-use crate::{Pallet as TokenizedOptions} ;
+use crate::{Pallet as TokenizedOptions, self as pallet_tokenized_options};
 
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
-use frame_system::RawOrigin;
 use codec::{Decode, Encode, MaxEncodedLen};
-use frame_system::{EventRecord};
+use frame_system::{EventRecord, Pallet as System, RawOrigin};
 use composable_traits::{
 	vault::{VaultConfig},
+	defi::{DeFiComposableConfig},
+	oracle::Price
 };
 use std::collections::BTreeMap;
 use sp_runtime::Perquintill;
 use frame_support::{
-	traits::{
+	traits::{EnsureOrigin,
 		fungible::{Inspect as NativeInspect, Transfer as NativeTransfer},
 		fungibles::{Inspect, InspectHold, Mutate, MutateHold, Transfer},
 	},
 };
+use frame_system::{pallet_prelude::*};
+
+fn encode_decode<D: Decode, E: Encode>(value: E) -> D {
+	let asset_id = value.encode();
+	let asset_id = D::decode(&mut &asset_id[..]).unwrap();
+	asset_id
+}
+
+
+// fn set_oracle_price<T: pallet_oracle::Config>(asset_id: AssetIdOf<T>, balance: BalanceOf<T>) {
+// 	let price = Price { price: balance, block: System::block_number() };
+// 	pallet_oracle::Prices::<T>::insert(asset_id, price);
+// }
+
+
+fn set_oracle_price<T: Config + pallet_oracle::Config>(asset_id: T::MayBeAssetId, price: u64) {
+	let asset_id: T::AssetId = encode_decode(asset_id);
+
+	pallet_oracle::Prices::<T>::insert(
+		asset_id,
+		Price { price: <T as pallet_oracle::Config>::PriceValue::from(price), block: 0_u32.into() },
+	);
+}
+
 
 
 fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
@@ -28,8 +53,11 @@ fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
 	assert_eq!(event, &system_event);
 }
 
+const UNIT: u64 = 10u64.pow(12);
 const A: u128 = 2;
 const B: u128 = 2000;
+const C: u128 = 131;
+
 
 // if to make it generic, and pass u128, it will pass HasCompact, and u128 will be 5 bits, not 16...
 pub fn recode_unwrap_u128<
@@ -46,8 +74,21 @@ pub fn recode_unwrap_u128<
 
 
 benchmarks! {
-	create_asset_vault_benchmark {
-		let caller: T::AccountId = whitelisted_caller();
+	where_clause {
+		where
+			T: pallet_tokenized_options::Config
+				+ DeFiComposableConfig 
+				+ frame_system::Config
+				+ pallet_oracle::Config
+	}
+	create_asset_vault {
+		let caller: <T as frame_system::Config>::AccountId = whitelisted_caller::<T::AccountId>();
+		// let origin = OriginFor::<T>::from(RawOrigin::Signed(caller));
+		let origin = OriginFor::<T>::from(RawOrigin::Root);
+
+		set_oracle_price::<T>(recode_unwrap_u128(B), 50_000 * UNIT);
+		set_oracle_price::<T>(recode_unwrap_u128(C), UNIT);
+
 		T::Assets::mint_into(recode_unwrap_u128(A), &caller, 2u32.into())?;
 		T::Assets::mint_into(recode_unwrap_u128(B), &caller, 1u32.into())?;
 
@@ -57,35 +98,23 @@ benchmarks! {
 			reserved: Perquintill::one(),
 			strategies: BTreeMap::new(),
 		};
-	}: { 
+	}: {
 		TokenizedOptions::<T>::create_asset_vault(
-			Origin::Signed(caller.clone()), 
+			origin, 
 			vault_config,
-		)
+		)?
+	}
+	verify {
+		assert_last_event::<T>(Event::CreatedAssetVault {
+			vault_id: recode_unwrap_u128(1u128),
+            asset_id: recode_unwrap_u128(B),
+		}.into())
 	}
 
-	create_asset_vault {
-		let caller: T::AccountId = whitelisted_caller();
-		T::Assets::mint_into(recode_unwrap_u128(A), &caller, 2u32.into())?;
-		T::Assets::mint_into(recode_unwrap_u128(B), &caller, 1u32.into())?;
-
-		let vault_config: VaultConfig<T::AccountId, T::MayBeAssetId> = VaultConfig {
-			asset_id: recode_unwrap_u128(B),
-			manager: whitelisted_caller(),
-			reserved: Perquintill::one(),
-			strategies: BTreeMap::new(),
-		};
-	}: _(
-			RawOrigin::Signed(caller.clone()), 
-			vault_config,
-		)
 }
-
-
-
+	
 impl_benchmark_test_suite!(
 	TokenizedOptions,
 	crate::mock::runtime::ExtBuilder::default().build(),
 	crate::mock::runtime::MockRuntime,
 );
-  
