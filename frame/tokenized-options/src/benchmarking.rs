fe0efce306f8c95c3e15430c2f81631009b05c8c
Matteocolella1995 <84871477+Matteocolella1995@users.noreply.github.com>
2022-07-19 13:09:43 +0000

    CU-3703gb9 - Completed create_asset_vault extrinsic setup

--- frame/tokenized-options/src/benchmarking.rs
+++ frame/tokenized-options/src/benchmarking.rs
@@ -2,7 +2,7 @@
 // #![cfg(feature = "runtime-benchmarks")]
 
 use crate::*;
-use crate::{Pallet as TokenizedOptions, self as pallet_tokenized_options};
+use crate::{Pallet as TokenizedOptions, self as pallet_tokenized_options, types::*};
 
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use codec::{Decode, Encode, MaxEncodedLen};
@@ -20,46 +20,23 @@ use frame_support::{
 		fungibles::{Inspect, InspectHold, Mutate, MutateHold, Transfer},
 	},
 };
-use frame_system::{pallet_prelude::*};
-
-fn encode_decode<D: Decode, E: Encode>(value: E) -> D {
-	let asset_id = value.encode();
-	let asset_id = D::decode(&mut &asset_id[..]).unwrap();
-	asset_id
-}
-
-
-// fn set_oracle_price<T: pallet_oracle::Config>(asset_id: AssetIdOf<T>, balance: BalanceOf<T>) {
-// 	let price = Price { price: balance, block: System::block_number() };
-// 	pallet_oracle::Prices::<T>::insert(asset_id, price);
-// }
-
-
-fn set_oracle_price<T: Config + pallet_oracle::Config>(asset_id: T::MayBeAssetId, price: u64) {
-	let asset_id: T::AssetId = encode_decode(asset_id);
-
-	pallet_oracle::Prices::<T>::insert(
-		asset_id,
-		Price { price: <T as pallet_oracle::Config>::PriceValue::from(price), block: 0_u32.into() },
-	);
-}
+use frame_system::pallet_prelude::*;
 
 
 
-fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
-	let events = frame_system::Pallet::<T>::events();
-	let system_event: <T as frame_system::Config>::Event = generic_event.into();
-	let EventRecord { event, .. } = &events[events.len() - 1];
-	assert_eq!(event, &system_event);
-}
-
-const UNIT: u64 = 10u64.pow(12);
+// ----------------------------------------------------------------------------------------------------
+//		Helper functions
+// ----------------------------------------------------------------------------------------------------
+const UNIT: u128 = 10u128.pow(12);
 const A: u128 = 2;
 const B: u128 = 2000;
 const C: u128 = 131;
 
+fn encode_decode<D: Decode, E: Encode>(value: E) -> D {
+	let asset_id = value.encode();
+	D::decode(&mut &asset_id[..]).unwrap()
+}
 
-// if to make it generic, and pass u128, it will pass HasCompact, and u128 will be 5 bits, not 16...
 pub fn recode_unwrap_u128<
 	O: Decode + MaxEncodedLen + Encode,
 	I: Decode + MaxEncodedLen + Encode,
@@ -72,6 +49,41 @@ pub fn recode_unwrap_u128<
 	O::decode(&mut &raw.encode()[..]).unwrap()
 }
 
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	let events = frame_system::Pallet::<T>::events();
+	let system_event: <T as frame_system::Config>::Event = generic_event.into();
+	let EventRecord { event, .. } = &events[events.len() - 1];
+	assert_eq!(event, &system_event);
+}
+
+fn set_oracle_price<T: Config + pallet_oracle::Config>(asset_id: T::MayBeAssetId, price: u64) {
+	let asset_id: T::AssetId = encode_decode(asset_id);
+
+	pallet_oracle::Prices::<T>::insert(
+		asset_id,
+		Price { price: <T as pallet_oracle::Config>::PriceValue::from(price), block: 0_u32.into() },
+	);
+}
+
+fn vault_benchmarking_setup<T: Config + pallet_oracle::Config>(asset_id: T::MayBeAssetId, price: u64)  {
+	let origin = OriginFor::<T>::from(RawOrigin::Root);
+
+	set_oracle_price::<T>(asset_id, price * (UNIT as u64));
+
+
+	let vault_config: VaultConfig<T::AccountId, T::MayBeAssetId> = VaultConfig {
+		asset_id,
+		manager: whitelisted_caller(),
+		reserved: Perquintill::one(),
+		strategies: BTreeMap::new(),
+	};
+
+	TokenizedOptions::<T>::create_asset_vault(
+		origin, 
+		vault_config,
+	).unwrap();
+}
+
 
 benchmarks! {
 	where_clause {
@@ -81,16 +93,12 @@ benchmarks! {
 				+ frame_system::Config
 				+ pallet_oracle::Config
 	}
+	
 	create_asset_vault {
-		let caller: <T as frame_system::Config>::AccountId = whitelisted_caller::<T::AccountId>();
-		// let origin = OriginFor::<T>::from(RawOrigin::Signed(caller));
 		let origin = OriginFor::<T>::from(RawOrigin::Root);
 
-		set_oracle_price::<T>(recode_unwrap_u128(B), 50_000 * UNIT);
-		set_oracle_price::<T>(recode_unwrap_u128(C), UNIT);
-
-		T::Assets::mint_into(recode_unwrap_u128(A), &caller, 2u32.into())?;
-		T::Assets::mint_into(recode_unwrap_u128(B), &caller, 1u32.into())?;
+		set_oracle_price::<T>(recode_unwrap_u128(B), 50_000 * (UNIT as u64));
+		set_oracle_price::<T>(recode_unwrap_u128(C), UNIT as u64);
 
 		let vault_config: VaultConfig<T::AccountId, T::MayBeAssetId> = VaultConfig {
 			asset_id: recode_unwrap_u128(B),
@@ -111,8 +119,9 @@ benchmarks! {
 		}.into())
 	}
 
+
 }
-	
+
 impl_benchmark_test_suite!(
 	TokenizedOptions,
 	crate::mock::runtime::ExtBuilder::default().build(),
