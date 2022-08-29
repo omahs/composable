d438b4acf1e7bc7d617e67c9f81f73abe6e55ed2
aeryz <abdullaheryz@protonmail.com>
2022-08-29 13:15:56 +0200

    fix(xcvm): assets in xcvm core
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- xcvm/cosmwasm/contracts/interpreter/src/contract.rs
+++ xcvm/cosmwasm/contracts/interpreter/src/contract.rs
@@ -8,8 +8,9 @@ use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, XCVMProgram};
 use crate::state::{Config, CONFIG};
 use cw20::{BalanceResponse, Cw20Contract, Cw20ExecuteMsg, Cw20QueryMsg};
+use num::Zero;
 use xcvm_asset_registry::msg::{GetAssetContractResponse, QueryMsg as AssetRegistryQueryMsg};
-use xcvm_core::{Amount, Funds, Instruction, NetworkID};
+use xcvm_core::{Funds, Instruction, NetworkId};
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -79,19 +80,19 @@ pub fn interpret_call(encoded: Vec<u8>, response: Response) -> Result<Response, 
 }
 
 pub fn interpret_spawn(
-    network: NetworkID,
-    salt: Vec<u8>,
-    assets: Funds,
-    program: XCVMProgram,
-    response: Response,
+	network: NetworkId,
+	salt: Vec<u8>,
+	assets: Funds,
+	program: XCVMProgram,
+	response: Response,
 ) -> Result<Response, ContractError> {
-    #[derive(Serialize)]
-    struct SpawnEvent {
-        network: NetworkID,
-        salt: Vec<u8>,
-        assets: Funds,
-        program: XCVMProgram,
-    }
+	#[derive(Serialize)]
+	struct SpawnEvent {
+		network: NetworkId,
+		salt: Vec<u8>,
+		assets: Funds,
+		program: XCVMProgram,
+	}
 
     let data = SpawnEvent {
         network,
@@ -127,28 +128,23 @@ pub fn interpret_transfer(
         )?;
         let contract = Cw20Contract(cw20_address.addr.clone());
 
-        let transfer_amount = match amount {
-            Amount::Fixed(ref fixed) => {
-                if fixed.0 == 0 {
-                    return Err(ContractError::ZeroTransferAmount);
-                }
-                amount.apply(0)
-            }
-            Amount::Ratio(ratio) => {
-                if ratio == 0 {
-                    return Err(ContractError::ZeroTransferAmount);
-                }
-                let query_msg = Cw20QueryMsg::Balance {
-                    address: to.clone(),
-                };
-                let response: BalanceResponse =
-                    deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
-                        contract_addr: cw20_address.addr.clone().into_string(),
-                        msg: to_binary(&query_msg)?,
-                    }))?;
-                amount.apply(response.balance.into())
-            }
-        };
+		if amount.is_zero() {
+			return Err(ContractError::ZeroTransferAmount)
+		}
+
+		let transfer_amount = {
+			if amount.slope == 0 {
+				amount.intercept.0
+			} else {
+				let query_msg = Cw20QueryMsg::Balance { address: to.clone() };
+				let response: BalanceResponse =
+					deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
+						contract_addr: cw20_address.addr.clone().into_string(),
+						msg: to_binary(&query_msg)?,
+					}))?;
+				amount.apply(response.balance.into())
+			}
+		};
 
         response = response.add_message(contract.call(Cw20ExecuteMsg::Transfer {
             recipient: to.clone(),
@@ -156,5 +152,140 @@ pub fn interpret_transfer(
         })?);
     }
 
-    Ok(response)
+	Ok(response)
+}
+
+#[cfg(test)]
+mod tests {
+	use std::collections::BTreeMap;
+
+	use crate::msg::XCVMInstruction;
+
+	use super::*;
+	use cosmwasm_std::{
+		testing::{mock_dependencies, mock_env, mock_info, MockQuerier},
+		wasm_execute, Addr, Attribute, ContractResult, QuerierResult,
+	};
+
+	#[test]
+	fn proper_instantiation() {
+		let mut deps = mock_dependencies();
+
+		let msg = InstantiateMsg { registry_address: "addr".to_string() };
+		let info = mock_info("sender", &vec![]);
+
+		let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+		assert_eq!(0, res.messages.len());
+
+		// Make sure that the storage is empty
+		assert_eq!(
+			CONFIG.load(&deps.storage).unwrap(),
+			Config { registry_address: Addr::unchecked("addr") }
+		);
+	}
+
+	fn wasm_querier(_: &WasmQuery) -> QuerierResult {
+		Ok(ContractResult::Ok(
+			to_binary(&xcvm_asset_registry::msg::GetAssetContractResponse {
+				addr: Addr::unchecked("mock"),
+			})
+			.unwrap(),
+		))
+		.into()
+	}
+
+	#[test]
+	fn execute_transfer() {
+		let mut deps = mock_dependencies();
+		let mut querier = MockQuerier::default();
+		querier.update_wasm(wasm_querier);
+		deps.querier = querier;
+
+		let info = mock_info("sender", &vec![]);
+		let _ = instantiate(
+			deps.as_mut(),
+			mock_env(),
+			info.clone(),
+			InstantiateMsg { registry_address: "addr".into() },
+		)
+		.unwrap();
+
+		let program = XCVMProgram {
+			tag: vec![],
+			instructions: vec![XCVMInstruction::Transfer {
+				to: "asset".into(),
+				assets: Funds::from([(1, 1_u128)]),
+			}]
+			.into(),
+		};
+
+		let res = execute(deps.as_mut(), mock_env(), info.clone(), ExecuteMsg::Execute { program })
+			.unwrap();
+		let contract = Cw20Contract(Addr::unchecked("mock"));
+		let msg = contract
+			.call(Cw20ExecuteMsg::Transfer { recipient: "asset".into(), amount: 1_u128.into() })
+			.unwrap();
+
+		assert_eq!(res.messages[0].msg, msg);
+	}
+
+	#[test]
+	fn execute_call() {
+		let mut deps = mock_dependencies();
+
+		let info = mock_info("sender", &vec![]);
+		let _ = instantiate(
+			deps.as_mut(),
+			mock_env(),
+			info.clone(),
+			InstantiateMsg { registry_address: "addr".into() },
+		)
+		.unwrap();
+
+		let cosmos_msg: CosmosMsg =
+			wasm_execute("1234", &"hello world".to_string(), vec![]).unwrap().into();
+		let msg = serde_json_wasm::to_string(&cosmos_msg).unwrap();
+
+		let program = XCVMProgram {
+			tag: vec![],
+			instructions: vec![XCVMInstruction::Call { encoded: msg.as_bytes().into() }].into(),
+		};
+
+		let res = execute(deps.as_mut(), mock_env(), info.clone(), ExecuteMsg::Execute { program })
+			.unwrap();
+		assert_eq!(res.messages[0].msg, cosmos_msg);
+	}
+
+	#[test]
+	fn execute_spawn() {
+		let mut deps = mock_dependencies();
+
+		let info = mock_info("sender", &vec![]);
+		let _ = instantiate(
+			deps.as_mut(),
+			mock_env(),
+			info.clone(),
+			InstantiateMsg { registry_address: "addr".into() },
+		)
+		.unwrap();
+
+		let program = XCVMProgram {
+			tag: vec![],
+			instructions: vec![XCVMInstruction::Spawn {
+				network: NetworkId(1),
+				salt: vec![],
+				assets: Funds(BTreeMap::new()),
+				program: XCVMProgram {
+					tag: vec![],
+					instructions: vec![XCVMInstruction::Call { encoded: vec![] }].into(),
+				},
+			}]
+			.into(),
+		};
+
+		let res = execute(deps.as_mut(), mock_env(), info.clone(), ExecuteMsg::Execute { program })
+			.unwrap();
+		assert_eq!(res.attributes[0], Attribute { key: "spawn".to_string(), value: r#"{"network":1,"salt":[],"assets":{},"program":{"tag":[],"instructions":[{"call":{"encoded":[]}}]}}"#.to_string() });
+	}
+>>>>>>> 2486c18e56 (fix(xcvm): assets in xcvm core)
 }
