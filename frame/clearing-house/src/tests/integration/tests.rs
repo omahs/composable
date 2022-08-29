308a724b86b7460cce2ace0944a41775eada5de2
Angelo Gregorio Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-08-29 13:38:20 +0000

    test(CH): ensure can't liquidate if above PMR

--- frame/clearing-house/src/tests/integration/tests.rs
+++ frame/clearing-house/src/tests/integration/tests.rs
@@ -7,7 +7,7 @@ use crate::{
 	},
 	tests::helpers,
 	Direction::{Long, Short},
-	Event, Error, Market, MarketConfig as MarketConfigGeneric,
+	Error, Event, Market, MarketConfig as MarketConfigGeneric,
 };
 
 use composable_support::validation::Validated;
@@ -15,7 +15,7 @@ use composable_traits::{
 	time::{DurationSeconds, ONE_HOUR},
 	vamm::Vamm as VammTrait,
 };
-use frame_support::{assert_ok, pallet_prelude::Hooks, traits::fungibles::Transfer};
+use frame_support::{assert_noop, assert_ok, pallet_prelude::Hooks, traits::fungibles::Transfer};
 use pallet_vamm::VammStateOf;
 use proptest::prelude::*;
 use sp_runtime::{traits::Zero, FixedPointNumber, Percent};
@@ -726,6 +726,56 @@ mod update_funding {
 
 mod liquidate {
 	use super::*;
+
+	#[test]
+	fn should_not_liquidate_if_above_partial_margin_ratio() {
+		ExtBuilder {
+			native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+			balances: vec![(ALICE, USDC, UNIT * 10), (BOB, USDC, UNIT * 1_000_000)],
+			..Default::default()
+		}
+		.build()
+		.execute_with(|| {
+			set_partial_liquidation_close((25, 100).into());
+			set_partial_liquidation_penalty((25, 1000).into());
+			set_liquidator_share_partial((50, 100).into());
+
+			let asset_id = DOT;
+			set_oracle_for(asset_id, 1_000);
+
+			let config = MarketConfig {
+				vamm_config: VammConfig {
+					base_asset_reserves: UNIT * 10_000,
+					quote_asset_reserves: UNIT * 100_000,
+					peg_multiplier: 1,
+					twap_period: ONE_HOUR,
+				},
+				margin_ratio_initial: (100, 1000).into(), // 10x max leverage
+				margin_ratio_partial: (99, 1000).into(),  // ~10.1x max leverage
+				margin_ratio_maintenance: (80, 1000).into(),
+				..Default::default()
+			};
+			assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
+
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(ALICE), USDC, UNIT * 10));
+
+			let market_id = Zero::zero();
+
+			// Alice goes long with maximum leverage
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(ALICE),
+				market_id,
+				Long,
+				UNIT * 100,
+				0
+			));
+
+			assert_noop!(
+				TestPallet::liquidate(Origin::signed(BOB), ALICE),
+				Error::<Runtime>::SufficientCollateral
+			);
+		})
+	}
 
 	#[test]
 	fn should_liquidate_if_below_partial_margin_ratio_by_pnl() {
