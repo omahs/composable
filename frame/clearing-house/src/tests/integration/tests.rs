2b79cddb83474279f3819bbb40982909d40815e9
Angelo Gregorio Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-08-30 13:56:09 +0000

    test(CH): verify failures after close_market

--- frame/clearing-house/src/tests/integration/tests.rs
+++ frame/clearing-house/src/tests/integration/tests.rs
@@ -154,6 +154,10 @@ fn get_unrealized_pnl(account_id: &AccountId, market_id: &MarketId) -> Decimal {
 	pnl
 }
 
+fn get_time_now() -> DurationSeconds {
+	<Timestamp as UnixTime>::now().as_secs()
+}
+
 fn set_maximum_oracle_mark_divergence(fraction: Decimal) {
 	helpers::set_maximum_oracle_mark_divergence::<Runtime>(fraction)
 }
@@ -950,7 +954,7 @@ mod close_market {
 				assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
 
 				let market_id = Zero::zero();
-				let now = <Timestamp as UnixTime>::now().as_secs();
+				let now = get_time_now();
 				assert_noop!(
 					TestPallet::close_market(
 						Origin::signed(BOB),
@@ -969,5 +973,100 @@ mod close_market {
 				let vamm = get_vamm(&market.vamm_id);
 				assert_eq!(vamm.closed, Some(now + 10));
 			})
+	}
+
+	#[test]
+	fn should_block_closing_positions_only_after_market_close() {
+		ExtBuilder {
+			native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+			balances: vec![(ALICE, USDC, UNIT * 100), (BOB, USDC, UNIT * 100)],
+			..Default::default()
+		}
+		.build()
+		.execute_with(|| {
+			let asset_id = DOT;
+			set_oracle_for(asset_id, 1_000);
+
+			let config = MarketConfig::default();
+			assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
+
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(ALICE), USDC, UNIT * 100));
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(BOB), USDC, UNIT * 100));
+
+			let market_id = Zero::zero();
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(ALICE),
+				market_id,
+				Long,
+				UNIT * 100,
+				0
+			));
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(BOB),
+				market_id,
+				Short,
+				UNIT * 100,
+				Balance::MAX
+			));
+
+			let now = get_time_now();
+			assert_ok!(TestPallet::close_market(Origin::root(), market_id, now + 12));
+
+			advance_blocks_by(1, 6);
+			assert_ok!(TestPallet::close_position(Origin::signed(ALICE), market_id));
+
+			advance_blocks_by(1, 6);
+			assert_noop!(
+				TestPallet::close_position(Origin::signed(BOB), market_id),
+				Error::<Runtime>::MarketClosed
+			);
+		})
+	}
+
+	#[test]
+	fn should_not_allow_opening_positions_after_close_market_call() {
+		ExtBuilder {
+			native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+			balances: vec![(ALICE, USDC, UNIT * 100), (BOB, USDC, UNIT * 100)],
+			..Default::default()
+		}
+		.build()
+		.execute_with(|| {
+			let asset_id = DOT;
+			set_oracle_for(asset_id, 1_000);
+
+			let config = MarketConfig::default();
+			assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
+
+			let market_id = Zero::zero();
+			let now = get_time_now();
+			assert_ok!(TestPallet::close_market(Origin::root(), market_id, now + 12));
+
+			advance_blocks_by(1, 6);
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(ALICE), USDC, UNIT * 100));
+			assert_noop!(
+				TestPallet::open_position(
+					Origin::signed(ALICE),
+					market_id,
+					Long,
+					UNIT * 100,
+					0
+				),
+				Error::<Runtime>::MarketShuttingDown
+			);
+
+			advance_blocks_by(1, 6);
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(BOB), USDC, UNIT * 100));
+			assert_noop!(
+				TestPallet::open_position(
+					Origin::signed(BOB),
+					market_id,
+					Short,
+					UNIT * 100,
+					Balance::MAX
+				),
+				Error::<Runtime>::MarketClosed
+			);
+		})
 	}
 }
