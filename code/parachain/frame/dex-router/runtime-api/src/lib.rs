88bdca3d46caf5994267c0fe727cbd296b2ff679
Connor Davis <17688291+PoisonPhang@users.noreply.github.com>
2022-09-16 10:20:41 -0500

    Created runtime-api for dex-router

---
+++ code/parachain/frame/dex-router/runtime-api/src/lib.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::Codec;
+use composable_support::rpc_helpers::SafeRpcWrapper;
+use composable_traits::dex::{PriceAggregate, RemoveLiquiditySimulationResult};
+use sp_std::collections::btree_map::BTreeMap;
+
+// Dex Rounter Runtime API declaration. Implemented for each runtime at
+// `runtime/<runtime-name>/src/lib.rs`.
+sp_api::decl_runtime_apis! {
+	pub trait DexRouterRuntimeApi<> {
+		fn fee_for_indirect_pool();
+	}
+}
