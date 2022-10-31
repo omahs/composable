fea3154489ed17d2dc3cedbd44064be83d018168
cor <cor@pruijs.dev>
2022-10-31 11:41:01 +0100

    fix: cleanup generated subxt-exports

--- code/utils/subxt-exports/src/lib.rs
+++ code/utils/subxt-exports/src/lib.rs
@@ -1,6 +1,12 @@
 mod generated;
 
+// Composable parachains
+#[rustfmt::skip] pub use generated::dali;
+#[rustfmt::skip] pub use generated::picasso;
+#[rustfmt::skip] pub use generated::composable;
 
-pub use generated::picasso;
+// Parity relaychains
+#[rustfmt::skip] pub use generated::rococo;
+#[rustfmt::skip] pub use generated::kusama;
+#[rustfmt::skip] pub use generated::polkadot;
 
-// TODO(expose the module that needs to be )
