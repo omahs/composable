39f5dd586c05c6077da5becf844b860032cf424b
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-11-26 23:09:28 +0000

    network

--- cosmwasm/contracts/xcm-executor/src/contract/execute.rs
+++ cosmwasm/contracts/xcm-executor/src/contract/execute.rs
@@ -1,15 +1,26 @@
+use cosmwasm_std::Api;
 use cosmwasm_std::{
     entry_point, to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdError,
     StdResult,
 };
 
-use crate::prelude::*;
 use crate::msg::*;
+use crate::prelude::*;
 
 pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> StdResult<Response> {
+    let origin = deps.api.addr_canonicalize(info.sender.as_str())?;
     match msg {
         ExecuteMsg::ExecuteXcmInCredit { msg } => {
-            use parity_scale_codec::Decode;
+            let origin = MultiLocation {
+                parents: 0,
+                interior: X1(AccountId32 {
+                    network: None,
+                    id: origin.0 .0.try_into().map_err(|_| StdError::ParseErr {
+                        target_type: "AccountId32".to_string(),
+                        msg: "Only AccountId32 origin are supported now".to_string(),
+                    })?,
+                }),
+            };
             // let k = xcm::v3::Junction::Parachain(42);
             // let k = xcm::v3::Junction::GeneralKey([42;32]);
             // let k = xcm::v3::Junction::GeneralIndex(42);
@@ -21,7 +32,7 @@ pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> S
                 }
             })?;
             let xcm = match xcm {
-                xcm::VersionedXcm::V2(xcm) => xcm,
+                VersionedXcm::V3(xcm) => {}
                 _ => Err(StdError::GenericErr {
                     msg: "Not supported version".to_string(),
                 })?,
--- cosmwasm/contracts/xcm-executor/src/prelude.rs
+++ cosmwasm/contracts/xcm-executor/src/prelude.rs
@@ -1,3 +1,19 @@
-pub use parity_scale_codec::alloc::string::{ToString, String};
+pub use parity_scale_codec::alloc::string::{String, ToString};
 pub use parity_scale_codec::alloc::vec::Vec;
+pub use parity_scale_codec::Decode;
 pub use serde::{Deserialize, Serialize};
+pub use cosmwasm_std::{Api};
+pub use xcm::{
+    v3::{
+        Error as XcmError, ExecuteXcm,
+        NetworkId,
+        Instruction::{self, *},
+        Junction,
+        Junction::AccountId32,
+        Junctions::{self, Here, X1, X2, X3},
+        MultiAssets, MultiLocation, Outcome, Response, SendXcm, Weight, Xcm,
+        XcmHash,
+        
+    },
+    VersionedXcm,
+};
